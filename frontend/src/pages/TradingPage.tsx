48ab73b2fb6cc79825cc32a82d984b717633d9f1
LucioJamieAI <148231849+JadoreThompson@users.noreply.github.com>
2025-10-17 17:18:41 +0100

    created order for and changed theme

--- frontend/src/pages/TradingPage.tsx
+++ frontend/src/pages/TradingPage.tsx
@@ -1,3 +1,4 @@
+import OrderForm from "@/components/order-form";
 import OrderBook from "@/components/orderbook";
 import RecentTrades from "@/components/recent-trades";
 import { useState, type FC } from "react";
@@ -10,37 +11,43 @@ const TradingPage: FC = () => {
       <header className="z-[2] h-10 w-full fixed top-0 left-0 bg-white">
         Header
       </header>
-      <main className="grid grid-cols-4 ro mt-10 pb-5 bg-gray-700">
-        <div className="h-full col-span-3">
+      <main className="grid grid-cols-10 ro mt-10 pb-5">
+        <div className="h-full col-span-8">
           <div className="h-15 w-full flex bg-green-500">a</div>
           <div className="h-120 w-full flex gap-1">
             <div className="h-full w-3/4 bg-pink-500"></div>
             <div className="h-full w-1/4 p-1">
               <div className="w-full flex gap-1">
                 <span
-                onClick={() => setShowOrderbook(!showOrderBook)}
+                  onClick={() => setShowOrderbook(!showOrderBook)}
                   className={`px-2 text-sm font-bold border-b-2 cursor-pointer ${
-                    showOrderBook ? "border-b-white" : "border-b-transparent text-muted-foreground"
+                    showOrderBook
+                      ? "border-b-white"
+                      : "border-b-transparent text-muted-foreground"
                   }`}
                 >
                   Order book
                 </span>
                 <span
-                onClick={() => setShowOrderbook(!showOrderBook)}
+                  onClick={() => setShowOrderbook(!showOrderBook)}
                   className={`px-2 text-sm font-bold border-b-2 cursor-pointer ${
-                    showOrderBook ? "border-b-transparent text-muted-foreground" : "border-b-white"
+                    showOrderBook
+                      ? "border-b-transparent text-muted-foreground"
+                      : "border-b-white"
                   }`}
                 >
                   Market trades
                 </span>
               </div>
-              {showOrderBook ? <OrderBook />: <RecentTrades />}
+              {showOrderBook ? <OrderBook /> : <RecentTrades />}
             </div>
           </div>
           <div className="h-120 flex-1 bg-blue-500">a</div>
         </div>
-        <div className="h-full col-span-1 flex flex-col bg-red-500">
-          <div className="h-120 w-full mb-1 bg-blue-200"></div>
+        <div className="h-full col-span-2 flex flex-col">
+          <div className="h-fit w-full mb-1">
+            <OrderForm />
+          </div>
           <div className="h-120 w-full bg-blue-200"></div>
         </div>
       </main>
